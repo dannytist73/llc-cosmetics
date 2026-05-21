4857beb8eb11b1c6bea8f88066de046001daa38a
Danny Boy Ngipol <dannyboy.ngipol@flexilabs.co>
2026-05-21 12:16:03 +0800

    fix: add writing-mode-vertical utility, safelist clinic grid classes, commit lockfile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js}'],
+  safelist: [
+    'clinic-brisbane',
+    'clinic-sunnybank',
+    'clinic-southport',
+    'clinic-robina',
+  ],
   theme: {
     extend: {
       colors: {
